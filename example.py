16c98c6f64e46a974523e6bda45155ba9c05d8fa
Brandon Hall <brandon.hall@arrow.com>
2018-12-31 14:27:14 -0700

    Create example.py

---
+++ example.py
@@ -0,0 +1,18 @@
+# Using Thunderboard with Python
+import pexpect
+import time
+ 
+DEVICE = "00:0B:57:0C:71:CD"
+ 
+# Run gatttool interactively.
+child = pexpect.spawn("gatttool -I")
+ 
+# Connect to the device.
+child.sendline("connect {0}".format(DEVICE))
+child.expect("Connection successful", timeout=5)
+ 
+#while True:
+child.sendline("char-read-hnd 0x0022")
+child.expect("Characteristic value/descriptor: ", timeout=10)
+child.expect("\r\n", timeout=10)
+print(child.before)
